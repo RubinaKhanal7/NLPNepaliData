2a43871dc1bb2096d05a7b792e3e6db71d13ed9e
RubinaKhanal7 <rubikhanal8@gmail.com>
2025-03-27 09:30:18 +0545

    changes in code

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ model = pickle.load(open('NepaliData.pickle', 'rb'))
 news_text = st.text_area("Enter Nepali News Article", height=250)
 
 if st.button('Predict Category'):
-    if news_text:
-        news_data = pd.DataFrame({'text': [news_text]})
-        prediction = model.predict(news_data)[0]
-        st.success(f"Predicted Category: {prediction}")
+    if news_text.strip():
+        df = pd.DataFrame({'news_text': [news_text]})  
+        prediction = model.predict(df['news_text'])[0]  
+        st.success(f"Predicted Category: {prediction}") 
     else:
-        st.warning("Please enter some text")
+        st.warning("Please enter some text") 
